7e2687a31562ac9a7efd4e9cd30e360c5578adae
mdx <mdx@mdx.pl>
2015-10-07 01:13:01 +0200

    img pattern

--- gallery/js/app.js
+++ gallery/js/app.js
@@ -1,74 +1,108 @@
-var canvas = window._canvas = new fabric.Canvas('appCanvas');
+$(function () {
+    var canvas = window._canvas = new fabric.Canvas('appCanvas');
+    var pattern;
 
-fabric.Object.prototype.set({
-    transparentCorners: false,
-    cornerColor: 'rgba(0,0,0,0.2)',
-    cornerSize: 10,
-    padding: 0
-});
+    fabric.Object.prototype.set({
+        transparentCorners: false,
+        cornerColor: 'rgba(0,0,0,0.5)',
+        cornerSize: 10,
+        padding: 0,
+        stroke: 2
+    });
 
-var mode = "add",
-    currentShape;
+    var mode = "add",
+        currentShape;
 
-canvas.observe("mouse:move", function (event) {
-    var pos = canvas.getPointer(event.e);
-    if (mode === "edit" && currentShape) {
-        var points = currentShape.get("points");
-        points[points.length - 1].x = pos.x - currentShape.get("left");
-        points[points.length - 1].y = pos.y - currentShape.get("top");
-        currentShape.set({
-            points: points
-        });
-        canvas.renderAll();
-    }
-});
-
-canvas.observe("mouse:down", function (event) {
-    var pos = canvas.getPointer(event.e);
-
-    if (mode === "add") {
-        var polygon = new fabric.Polygon([{
-            x: pos.x,
-            y: pos.y
-        }, {
-            x: pos.x + 0.5,
-            y: pos.y + 0.5
-        }], {
-            fill: 'black',
-            opacity: 0.2,
-            selectable: false,
-            borderColor: 'red',
-            centeredRotation: true,
-            hasControls: true
-        });
-        currentShape = polygon;
-        canvas.add(currentShape);
-        mode = "edit";
-    } else if (mode === "edit" && currentShape && currentShape.type === "polygon") {
-        var points = currentShape.get("points");
-        points.push({
-            x: pos.x - currentShape.get("left"),
-            y: pos.y - currentShape.get("top")
-        });
-        currentShape.set({
-            points: points
-        });
-        canvas.renderAll();
-    }
-});
-
-fabric.util.addListener(window, 'keyup', function (e) {
-    if (e.keyCode === 27) {
-        if (mode === 'edit' || mode === 'add') {
-            mode = 'normal';
+    canvas.observe("mouse:move", function (event) {
+        var pos = canvas.getPointer(event.e);
+        if (mode === "edit" && currentShape) {
+            var points = currentShape.get("points");
+            points[points.length - 1].x = pos.x - currentShape.get("left");
+            points[points.length - 1].y = pos.y - currentShape.get("top");
             currentShape.set({
-                selectable: true
+                points: points
             });
-            currentShape._calcDimensions(false);
-            currentShape.setCoords();
-        } else {
+            canvas.renderAll();
+        }
+    });
+
+    canvas.observe("mouse:down", function (event) {
+        var pos = canvas.getPointer(event.e);
+
+        if (mode === "add") {
+            var polygon = new fabric.Polygon([{
+                x: pos.x,
+                y: pos.y
+            }, {
+                x: pos.x + 0.5,
+                y: pos.y + 0.5
+            }], {
+                opacity: 1,
+                selectable: false,
+                borderColor: 'red',
+                originX: 'center',
+                originY: 'center',
+                fill: pattern
+            });
+            currentShape = polygon;
+            canvas.add(currentShape);
+            mode = "edit";
+        } else if (mode === "edit" && currentShape && currentShape.type === "polygon") {
+            var points = currentShape.get("points");
+            points.push({
+                x: pos.x - currentShape.get("left"),
+                y: pos.y - currentShape.get("top")
+            });
+            currentShape.set({
+                points: points
+            });
+            canvas.renderAll();
+        }
+    });
+
+    fabric.util.addListener(window, 'keyup', function (e) {
+        if (e.keyCode === 27) {
+            if (mode === 'edit' || mode === 'add') {
+                mode = 'normal';
+                currentShape.set({
+                    selectable: true
+                });
+                currentShape._calcDimensions(false);
+                currentShape.setCoords();
+            } else {
+                mode = 'add';
+            }
+            currentShape = null;
+        }
+
+        if (e.keyCode === 46 || e.keyCode === 8) {
+            canvas.item(0).remove();
+            currentShape = null;
             mode = 'add';
         }
-        currentShape = null;
+
+    });
+
+    function loadTexture(url) {
+        fabric.Image.fromURL(url, function (img) {
+            var patternSourceCanvas = new fabric.StaticCanvas();
+            patternSourceCanvas.add(img);
+            pattern = new fabric.Pattern({
+                source: function () {
+                    patternSourceCanvas.setDimensions({
+                        width: img.getWidth(),
+                        height: img.getHeight()
+                    });
+                    return patternSourceCanvas.getElement();
+                },
+                repeat: 'repeat'
+            });
+        });
     }
-})
+
+    $('.images img').click(function () {
+        $('.images img').css('border', 0);
+        loadTexture($(this).data('url'))
+        $(this).css('border', '1px solid red');
+    });
+});
