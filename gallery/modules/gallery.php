d78601fecd884b90cdafd2aa17f4384758447eac
mdx <mdx@mdx.pl>
2015-12-03 21:50:34 +0100

    FEATURE 9. Wydaje mi się że powinna być jeszcze jedna opcja, tam w tym dopasowaniu, tak aby można było wyłączyć duplikowanie zdjęć aby było tylko jedno bez powtórzeń. Da radę to zrobić ?

--- gallery/modules/gallery.php
+++ gallery/modules/gallery.php
@@ -3,12 +3,12 @@ include('../admin/config.php');
 include('../admin/mysql.open.php');
 include('../admin/functions.php');
 
-$albums = getAllAlbums('ASC');
+$albums = getAllAlbums('DESC');
 $response = [];
 foreach ($albums as $album) {
     $albumId = $album['AlbumID'];
     $response[$albumId] = $album;
-    $response[$albumId]['images'] = getAlbumImages($albumId, 'ASC');
+    $response[$albumId]['images'] = getAlbumImages($albumId, 'DESC');
 }
 include('../admin/mysql.close.php');
 echo json_encode($response);
